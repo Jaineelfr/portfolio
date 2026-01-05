dba96556fd1c8314203bcf3b3d0fe28b00fa751a
Jaineelfr <jaineelpatel710@gmail.com>
2026-01-04 22:30:23 -0500

    Add files via upload

---
+++ csharp-projects/pacman/pacman - Copy/pacman/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pacman
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pacman.Location = new Point(11, 11);
+            for (int x = 0; x < 667; x = x + 5)
+            {
+
+
+                pacman.Location = new Point(x, 11);
+                this.Update();
+                Thread.Sleep(50);
+
+                if (x % 2 == 0)
+                {
+                    pacman.BackgroundImage = Properties.Resources.pacMouthOpen;
+
+                }
+                else
+                {
+                    pacman.BackgroundImage = Properties.Resources.pacMouthClosed;
+                }
+
+
+            }
+
+            pacman.Location = new Point(667, 11);
+            for (int y = 0; y < 470; y = y + 5)
+            {
+
+
+                pacman.Location = new Point(667, y);
+                this.Update();
+                Thread.Sleep(50);
+
+                if (y % 2 == 0)
+                {
+                    pacman.BackgroundImage = Properties.Resources.pacMouthOpendown;
+
+                }
+                else
+                {
+                    pacman.BackgroundImage = Properties.Resources.pacMouthCloseddown;
+                }
+            }
+            pacman.Location = new Point(667, 470);
+            for (int x = 667; x > 0; x = x - 5)
+            {
+
+
+                pacman.Location = new Point(x, 470);
+                this.Update();
+                Thread.Sleep(50);
+
+                if (x % 2 == 0)
+                {
+                    pacman.BackgroundImage = Properties.Resources.pacMouthOpenLeft;
+
+                }
+                else
+                {
+                    pacman.BackgroundImage = Properties.Resources.pacMouthClosedleft;
+                }
+            }
+            pacman.Location = new Point(11, 470);
+            for (int y = 470; y > 0; y = y - 5)
+            {
+
+
+                pacman.Location = new Point(11, y);
+                this.Update();
+                Thread.Sleep(50);
+
+                if (y % 2 == 0)
+                {
+                    pacman.BackgroundImage = Properties.Resources.pacMouthOpenUp;
+
+                }
+                else
+                {
+                    pacman.BackgroundImage = Properties.Resources.pacMouthClosedup;
+                }
+                Application.Exit();
+            }
+        }
+    }
+}
---
+++ csharp-projects/pacman/pacman/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace pacman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pacman = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pacman)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pacman
+            // 
+            this.pacman.BackgroundImage = global::pacman.Properties.Resources.pacMouthClosed;
+            this.pacman.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pacman.Location = new System.Drawing.Point(13, 13);
+            this.pacman.Margin = new System.Windows.Forms.Padding(4);
+            this.pacman.Name = "pacman";
+            this.pacman.Size = new System.Drawing.Size(118, 119);
+            this.pacman.TabIndex = 0;
+            this.pacman.TabStop = false;
+            this.pacman.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1474, 1044);
+            this.Controls.Add(this.pacman);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pacman)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pacman;
+    }
+}
+
