29642e132d1e5d62dd1153af25e1728e69a10d7f
Jaineelfr <jaineelpatel710@gmail.com>
2026-01-04 22:24:48 -0500

    Add files via upload

---
+++ csharp-projects/slotmachine/slotmachine/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace slotmachine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbl_title = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_pull = new System.Windows.Forms.Button();
+            this.lbl_points = new System.Windows.Forms.Label();
+            this.lbl_score = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pb_arash_3 = new System.Windows.Forms.PictureBox();
+            this.pb_arash_2 = new System.Windows.Forms.PictureBox();
+            this.pb_arash = new System.Windows.Forms.PictureBox();
+            this.pb_herass = new System.Windows.Forms.PictureBox();
+            this.pb_arash2 = new System.Windows.Forms.PictureBox();
+            this.pb_arashgay = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_arash_3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_arash_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_arash)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_herass)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_arash2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_arashgay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_title
+            // 
+            this.lbl_title.AutoSize = true;
+            this.lbl_title.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 19.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_title.Location = new System.Drawing.Point(294, 22);
+            this.lbl_title.Name = "lbl_title";
+            this.lbl_title.Size = new System.Drawing.Size(723, 53);
+            this.lbl_title.TabIndex = 3;
+            this.lbl_title.Text = "UGLY ARASH SLOT MACHINE!!";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(432, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(405, 32);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "even when u win, u lose";
+            // 
+            // btn_pull
+            // 
+            this.btn_pull.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn_pull.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_pull.Location = new System.Drawing.Point(125, 530);
+            this.btn_pull.Name = "btn_pull";
+            this.btn_pull.Size = new System.Drawing.Size(1046, 148);
+            this.btn_pull.TabIndex = 5;
+            this.btn_pull.Text = "PULL";
+            this.btn_pull.UseVisualStyleBackColor = false;
+            this.btn_pull.Click += new System.EventHandler(this.btn_pull_Click);
+            // 
+            // lbl_points
+            // 
+            this.lbl_points.AutoSize = true;
+            this.lbl_points.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_points.Location = new System.Drawing.Point(468, 757);
+            this.lbl_points.Name = "lbl_points";
+            this.lbl_points.Size = new System.Drawing.Size(133, 32);
+            this.lbl_points.TabIndex = 6;
+            this.lbl_points.Text = "POINTS:";
+            // 
+            // lbl_score
+            // 
+            this.lbl_score.AutoSize = true;
+            this.lbl_score.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl_score.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_score.Location = new System.Drawing.Point(621, 742);
+            this.lbl_score.Name = "lbl_score";
+            this.lbl_score.Size = new System.Drawing.Size(2, 53);
+            this.lbl_score.TabIndex = 7;
+            this.lbl_score.Click += new System.EventHandler(this.lbl_score_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(468, 757);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 32);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "POINTS:";
+            // 
+            // pb_arash_3
+            // 
+            this.pb_arash_3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pb_arash_3.BackgroundImage")));
+            this.pb_arash_3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pb_arash_3.InitialImage = null;
+            this.pb_arash_3.Location = new System.Drawing.Point(867, 149);
+            this.pb_arash_3.Name = "pb_arash_3";
+            this.pb_arash_3.Size = new System.Drawing.Size(304, 341);
+            this.pb_arash_3.TabIndex = 2;
+            this.pb_arash_3.TabStop = false;
+            // 
+            // pb_arash_2
+            // 
+            this.pb_arash_2.BackgroundImage = global::slotmachine.Properties.Resources.arash21;
+            this.pb_arash_2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pb_arash_2.InitialImage = null;
+            this.pb_arash_2.Location = new System.Drawing.Point(497, 149);
+            this.pb_arash_2.Name = "pb_arash_2";
+            this.pb_arash_2.Size = new System.Drawing.Size(304, 341);
+            this.pb_arash_2.TabIndex = 1;
+            this.pb_arash_2.TabStop = false;
+            // 
+            // pb_arash
+            // 
+            this.pb_arash.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pb_arash.BackgroundImage")));
+            this.pb_arash.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pb_arash.InitialImage = null;
+            this.pb_arash.Location = new System.Drawing.Point(125, 149);
+            this.pb_arash.Name = "pb_arash";
+            this.pb_arash.Size = new System.Drawing.Size(304, 341);
+            this.pb_arash.TabIndex = 0;
+            this.pb_arash.TabStop = false;
+            this.pb_arash.Click += new System.EventHandler(this.pb_arash_Click);
+            // 
+            // pb_herass
+            // 
+            this.pb_herass.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pb_herass.Location = new System.Drawing.Point(125, 149);
+            this.pb_herass.Name = "pb_herass";
+            this.pb_herass.Size = new System.Drawing.Size(304, 341);
+            this.pb_herass.TabIndex = 0;
+            this.pb_herass.TabStop = false;
+            // 
+            // pb_arash2
+            // 
+            this.pb_arash2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pb_arash2.Location = new System.Drawing.Point(497, 149);
+            this.pb_arash2.Name = "pb_arash2";
+            this.pb_arash2.Size = new System.Drawing.Size(304, 341);
+            this.pb_arash2.TabIndex = 1;
+            this.pb_arash2.TabStop = false;
+            // 
+            // pb_arashgay
+            // 
+            this.pb_arashgay.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pb_arashgay.Location = new System.Drawing.Point(867, 149);
+            this.pb_arashgay.Name = "pb_arashgay";
+            this.pb_arashgay.Size = new System.Drawing.Size(304, 341);
+            this.pb_arashgay.TabIndex = 2;
+            this.pb_arashgay.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1342, 876);
+            this.Controls.Add(this.lbl_score);
+            this.Controls.Add(this.lbl_points);
+            this.Controls.Add(this.btn_pull);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_title);
+            this.Controls.Add(this.pb_arash_3);
+            this.Controls.Add(this.pb_arash_2);
+            this.Controls.Add(this.pb_arash);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_arash_3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_arash_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_arash)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_herass)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_arash2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_arashgay)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pb_arash;
+        private System.Windows.Forms.PictureBox pb_arash_2;
+        private System.Windows.Forms.PictureBox pb_arash_3;
+        private System.Windows.Forms.Label lbl_title;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_pull;
+        private System.Windows.Forms.Label lbl_points;
+        private System.Windows.Forms.Label lbl_score;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pb_herass;
+        private System.Windows.Forms.PictureBox pb_arash2;
+        private System.Windows.Forms.PictureBox pb_arashgay;
+    }
+}
+
---
+++ csharp-projects/slotmachine/slotmachine/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace slotmachine
+{
+    public partial class Form1 : Form
+    {
+        Random box = new Random();
+        int num;
+        int num2;
+        int num3;
+        int points = 100;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_pull_Click(object sender, EventArgs e)
+        {
+            lbl_score.Text = points.ToString();
+            points = points - 10;
+            num = box.Next(1,4);
+            num2 = box.Next(1, 4);
+            num3 = box.Next(1, 4);
+            if(num==num2&&num2==num3)
+            {
+                num = box.Next(1, 4);
+                num2 = box.Next(1, 4);
+                num3 = box.Next(1, 4);
+            }
+            if (num == 1) 
+            { 
+                pb_arash.BackgroundImage = Properties.Resources.arash; 
+            }
+            else if (num == 2)
+            {
+                pb_arash.BackgroundImage = Properties.Resources.arash2;
+            }
+           else if (num == 3)
+            {
+                pb_arash.BackgroundImage = Properties.Resources.arash3;
+            }
+
+
+
+            
+            if (num2 == 1)
+            {
+                pb_arash_2.BackgroundImage = Properties.Resources.arash;
+            }
+            else if (num2 == 2)
+            {
+                pb_arash_2.BackgroundImage = Properties.Resources.arash2;
+            }
+            else if (num2 == 3)
+            {
+                pb_arash_2.BackgroundImage = Properties.Resources.arash3;
+            }
+
+
+
+            
+            if (num3 == 1)
+            {
+                pb_arash_3.BackgroundImage = Properties.Resources.arash;
+            }
+            else if (num3 == 2)
+            {
+                pb_arash_3.BackgroundImage = Properties.Resources.arash2;
+            }
+            else if (num3 == 3)
+            {
+                pb_arash_3.BackgroundImage = Properties.Resources.arash3;
+            }
+            if (num == 1 && num2 == 1 && num3 == 1)
+            {
+                points = points + 50;
+                MessageBox.Show("You Won 50 Points!");
+                lbl_score.Text = points.ToString();
+            }
+            if (num == 2 && num2 == 2 && num3 == 2)
+            {
+                points = points + 500;
+                MessageBox.Show("You Won 500 Points!");
+                lbl_score.Text = points.ToString();
+            }
+            if (num == 3 && num2 == 3 && num3 == 3)
+            {
+                points = points + 100;
+                MessageBox.Show("You Won 100 Points!");
+                lbl_score.Text = points.ToString();
+            }
+            if (points == 0)
+            {
+                lbl_score.Text = points.ToString();
+                MessageBox.Show("U are literally as dumb as Arash, and now you've lost ur all ur points. SO GO HOME AND CRY, I DONT CARE");
+                Application.Exit();
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl_score_Click(object sender, EventArgs e)
+        {
+            lbl_score.Text = points.ToString();
+        }
+
+        private void pb_arash_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
